406efab3099f59df0505c10da9ed02bd55d54253
MADANNAGARI RAJU <106908371+MadannagariRaju@users.noreply.github.com>
2024-07-05 12:48:00 +0530

    Time: 0 ms (100%), Space: 40.3 MB (72.22%) - LeetHub

--- 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.java
+++ 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.java
@@ -3,6 +3,6 @@ class Solution {
         int sum =arrivalTime+delayedTime;
         if(sum ==24) return 0;
         else if(sum<24) return sum;
-        else  return sum%24;
+        else  return sum%24; /* sum>24 eg : sum =25 then 25%24 =>1*/
     }
 }
