5f791b6a606af297cf3735d46e6036946506561f
MADANNAGARI RAJU <106908371+MadannagariRaju@users.noreply.github.com>
2024-07-05 11:59:58 +0530

    Time: 1 ms (100%), Space: 41.1 MB (72.18%) - LeetHub

--- 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.java
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.java
@@ -1,7 +1,22 @@
 class Solution {
     public int theMaximumAchievableX(int num, int t) {
-        for(int i=0;i<t;i++) num ++;
-        for(int i=0;i<t;i++) num++;
-        return num;
+
+        /*
+            eg: num = 3, t=2;
+            3+1+1 = 5;
+            X - 1-1 = 5;
+            X=5+1+1;
+
+            num_+1_+1-1_-1_X
+               ---t--|--t--
+               distance is 2t
+               X-num = 2t;
+               X = 2t+num;
+        */
+        // for(int i=0;i<t;i++) num ++;
+        // for(int i=0;i<t;i++) num++;
+        // return num;
+
+        return num + 2*t;
     }
 }
