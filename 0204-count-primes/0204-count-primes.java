3aa5fd4736f461b977a95c7a71651c348a8300d5
MADANNAGARI RAJU <106908371+MadannagariRaju@users.noreply.github.com>
2024-07-14 09:48:02 +0530

    Time: 102 ms (52.53%), Space: 64.4 MB (6.72%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,48 @@
+class Solution {
+   /* public boolean check(int n) {
+        int count=0;
+        for(int i=1;i<=Math.sqrt(n);i++) {
+            if(n%i ==0){
+                count++;
+                if((n/i !=i)) {
+                    count++;
+                }
+            }
+        }
+        if(count ==2) return true;
+        return false;
+    }
+    public int countPrimes(int n) {
+        int count=0;
+        for(int i=1;i<n;i++) {
+            if(check(i)){
+                // System.out.println(i);
+                count++;
+            }
+        }
+        return count;
+    }  */
+    public int countPrimes(int n) {
+        int primes[]= new int[n+1];
+        Arrays.fill(primes,1);
+        for(int i=2;i<=Math.sqrt(n);i++) {
+            if(primes[i] ==1) {
+                for(int j=i*i;j<=n;) {
+                    primes[j]=0;
+                    j+=i;
+                }
+            }
+        }
+        // System.out.println(Arrays.toString(primes));
+        int count=0;
+        // System.out.println(primes.length);
+        for(int i=2;i<primes.length-1;i++) {
+            // System.out.println(i);
+            if(primes[i] ==1) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
